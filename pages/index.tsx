bae16dafbb2b601c218ee416ab1c3c164c1f4087
Lucas-Palma <palmalucassantiago@gmail.com>
2023-06-02 02:43:35 -0300

    feat: movie components and movies endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { getSession, signOut } from "next-auth/react";
 import { NextPageContext } from 'next';
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,10 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
  
 export default function Home() {
+  const { data: movie = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movie} />
+      </div>
     </>
   )
 }
